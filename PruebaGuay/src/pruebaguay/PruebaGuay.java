533c3e4975f691a0bb5f1518824ec312eca6acc8
Andrés <Andrs@10.0.116.114>
2016-11-23 20:02:34 +0100

    añadido un sout criticando el repositorio

--- PruebaGuay/src/pruebaguay/PruebaGuay.java
+++ PruebaGuay/src/pruebaguay/PruebaGuay.java
@@ -17,7 +17,7 @@ public class PruebaGuay {
     public static void main(String[] args) {
         System.out.println("Hola equipo GUAY");
         System.out.println("Hola Aitor.");
-        
+        System.out.println("El repositorio guay tiene lagunas");
     }
     
 }
